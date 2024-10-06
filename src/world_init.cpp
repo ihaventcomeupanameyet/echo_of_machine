57eb14077a2e915fbb7e48a3718b8071d7a6a264
andiel13 <andie.lizo@gmail.com>
2024-10-06 15:08:49 -0700

    added player inventory input handling

---
+++ src/world_init.cpp
@@ -0,0 +1,88 @@
+#include "world_init.hpp"
+#include "tiny_ecs_registry.hpp"
+
+Entity createPlayer(RenderSystem* renderer, vec2 pos)
+{
+	auto entity = Entity();
+
+	// Setting initial motion values
+	Motion& motion = registry.motions.emplace(entity);
+	motion.position = pos;
+	motion.angle = 0.f;
+	motion.velocity = { 0.f, 0.f };
+	motion.scale = vec2({ PLAYER_BB_WIDTH, PLAYER_BB_HEIGHT });
+	//motion.scale.y *= -1; // point front to the right
+
+	// create an empty component for our character
+	Player& player = registry.players.emplace(entity);
+
+	Inventory& inventory = player.inventory;
+	// test items
+	inventory.addItem("Robot Hand", 2);
+	inventory.addItem("Key", 1);
+
+
+	registry.renderRequests.insert(
+		entity,
+		{ TEXTURE_ASSET_ID::PLAYER_IDLE,    
+		  EFFECT_ASSET_ID::TEXTURED,     
+		  GEOMETRY_BUFFER_ID::SPRITE });    
+
+
+	return entity;
+}
+
+
+Entity createRobot(RenderSystem* renderer, vec2 position)
+{
+	auto entity = Entity();
+
+	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
+	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::SPRITE);
+	registry.meshPtrs.emplace(entity, &mesh);
+
+	// Initialize the motion
+	auto& motion = registry.motions.emplace(entity);
+	motion.angle = 0.f;
+	motion.velocity = { 0.f, 0.f };
+	motion.position = position;
+	motion.scale.y *= -1;
+
+	// Setting initial values, scale is negative to make it face the opposite way
+	motion.scale = vec2({ -ROBOT_BB_WIDTH, ROBOT_BB_HEIGHT });
+
+	// create an empty Robot component to be able to refer to all robots
+	registry.robots.emplace(entity);
+	registry.renderRequests.insert(
+		entity,
+		{
+			TEXTURE_ASSET_ID::ROBOT,
+			EFFECT_ASSET_ID::TEXTURED,
+			GEOMETRY_BUFFER_ID::SPRITE
+		});
+
+	return entity;
+}
+
+Entity createLine(vec2 position, vec2 scale)
+{
+	Entity entity = Entity();
+
+	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
+	//registry.renderRequests.insert(
+	//	entity, {
+	//		TEXTURE_ASSET_ID::TEXTURE_COUNT,
+	//		EFFECT_ASSET_ID::EGG,
+	//		GEOMETRY_BUFFER_ID::DEBUG_LINE
+	//	});
+
+	// Create motion
+	Motion& motion = registry.motions.emplace(entity);
+	motion.angle = 0.f;
+	motion.velocity = { 0, 0 };
+	motion.position = position;
+	motion.scale = scale;
+
+	registry.debugComponents.emplace(entity);
+	return entity;
+}
---
+++ src/world_system.cpp
@@ -0,0 +1,332 @@
+// Header
+#include "world_system.hpp"
+#include "world_init.hpp"
+
+// stlib
+#include <cassert>
+#include <sstream>
+#include <iostream>
+
+#include "physics_system.hpp"
+
+// Game configuration
+const size_t MAX_NUM_ROBOTS = 15;
+const size_t ROBOT_SPAWN_DELAY_MS = 2000 * 3;;
+
+// create the world
+WorldSystem::WorldSystem()
+	: points(0)
+	, next_robot_spawn(0.f) {
+	// Seeding rng with random device
+	rng = std::default_random_engine(std::random_device()());
+}
+
+WorldSystem::~WorldSystem() {
+	
+	// destroy music components
+	if (background_music != nullptr)
+		Mix_FreeMusic(background_music);
+	if (player_dead_sound != nullptr)
+		Mix_FreeChunk(player_dead_sound);
+
+	Mix_CloseAudio();
+
+	// Destroy all created components
+	registry.clear_all_components();
+
+	// Close the window
+	glfwDestroyWindow(window);
+}
+
+// Debugging
+namespace {
+	void glfw_err_cb(int error, const char *desc) {
+		fprintf(stderr, "%d: %s", error, desc);
+	}
+}
+
+// World initialization
+// Note, this has a lot of OpenGL specific things, could be moved to the renderer
+GLFWwindow* WorldSystem::create_window() {
+	///////////////////////////////////////
+	// Initialize GLFW
+	glfwSetErrorCallback(glfw_err_cb);
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW");
+		return nullptr;
+	}
+
+	//-------------------------------------------------------------------------
+	// If you are on Linux or Windows, you can change these 2 numbers to 4 and 3 and
+	// enable the glDebugMessageCallback to have OpenGL catch your mistakes for you.
+	// GLFW / OGL Initialization
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+#if __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+	glfwWindowHint(GLFW_RESIZABLE, 0);
+
+	// Create the main window (for rendering, keyboard, and mouse input)
+	window = glfwCreateWindow(window_width_px, window_height_px, "Game Project", nullptr, nullptr);
+	if (window == nullptr) {
+		fprintf(stderr, "Failed to glfwCreateWindow");
+		return nullptr;
+	}
+
+	// Setting callbacks to member functions (that's why the redirect is needed)
+	// Input is handled using GLFW, for more info see
+	// http://www.glfw.org/docs/latest/input_guide.html
+	glfwSetWindowUserPointer(window, this);
+	auto key_redirect = [](GLFWwindow* wnd, int _0, int _1, int _2, int _3) { ((WorldSystem*)glfwGetWindowUserPointer(wnd))->on_key(_0, _1, _2, _3); };
+	auto cursor_pos_redirect = [](GLFWwindow* wnd, double _0, double _1) { ((WorldSystem*)glfwGetWindowUserPointer(wnd))->on_mouse_move({ _0, _1 }); };
+	glfwSetKeyCallback(window, key_redirect);
+	glfwSetCursorPosCallback(window, cursor_pos_redirect);
+
+	//////////////////////////////////////
+	// Loading music and sounds with SDL
+	if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+		fprintf(stderr, "Failed to initialize SDL Audio");
+		return nullptr;
+	}
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) == -1) {
+		fprintf(stderr, "Failed to open audio device");
+		return nullptr;
+	}
+
+	background_music = Mix_LoadMUS(audio_path("music.wav").c_str());
+	player_dead_sound = Mix_LoadWAV(audio_path("death_sound.wav").c_str());
+
+	if (background_music == nullptr || player_dead_sound == nullptr) {
+		fprintf(stderr, "Failed to load sounds\n %s\n %s\n %s\n make sure the data directory is present",
+			audio_path("music.wav").c_str(),
+			audio_path("death_sound.wav").c_str(),
+			audio_path("eat_sound.wav").c_str());
+		return nullptr;
+	}
+
+	return window;
+}
+
+void WorldSystem::init(RenderSystem* renderer_arg) {
+	this->renderer = renderer_arg;
+	// Playing background music indefinitely
+	Mix_PlayMusic(background_music, -1);
+	fprintf(stderr, "Loaded music\n");
+
+	// Set all states to default
+    restart_game();
+}
+
+// Update our game world
+bool WorldSystem::step(float elapsed_ms_since_last_update) {
+	// Updating window title with points
+	std::stringstream title_ss;
+	title_ss << "Points: " << points;
+	glfwSetWindowTitle(window, title_ss.str().c_str());
+
+	// Remove debug info from the last step
+	while (registry.debugComponents.entities.size() > 0)
+	    registry.remove_all_components_of(registry.debugComponents.entities.back());
+
+	// Removing out of screen entities
+	auto& motions_registry = registry.motions;
+
+	// Remove entities that leave the screen on the left side
+	// Iterate backwards to be able to remove without unterfering with the next object to visit
+	// (the containers exchange the last element with the current)
+	for (int i = (int)motions_registry.components.size()-1; i>=0; --i) {
+	    Motion& motion = motions_registry.components[i];
+		if (motion.position.x + abs(motion.scale.x) < 0.f) {
+			if(!registry.players.has(motions_registry.entities[i])) // don't remove the player
+				registry.remove_all_components_of(motions_registry.entities[i]);
+		}
+	}
+
+	// spawn new robots
+	next_robot_spawn -= elapsed_ms_since_last_update * current_speed;
+	if (registry.robots.components.size() <= MAX_NUM_ROBOTS && next_robot_spawn < 0.f) {
+		// reset timer
+		next_robot_spawn = (ROBOT_SPAWN_DELAY_MS / 2) + uniform_dist(rng) * (ROBOT_SPAWN_DELAY_MS / 2);
+
+		// create robots with random initial position
+        createRobot(renderer, vec2(50.f + uniform_dist(rng) * (window_width_px - 100.f), 0.f + uniform_dist(rng) * (window_width_px - 100.f)));
+	}
+
+
+	// Processing the player state
+	assert(registry.screenStates.components.size() <= 1);
+    ScreenState &screen = registry.screenStates.components[0];
+
+    float min_counter_ms = 3000.f;
+	for (Entity entity : registry.deathTimers.entities) {
+		// progress timer
+		DeathTimer& counter = registry.deathTimers.get(entity);
+		counter.counter_ms -= elapsed_ms_since_last_update;
+		if(counter.counter_ms < min_counter_ms){
+		    min_counter_ms = counter.counter_ms;
+		}
+
+		// restart the game once the death timer expired
+		if (counter.counter_ms < 0) {
+			registry.deathTimers.remove(entity);
+			screen.darken_screen_factor = 0;
+            restart_game();
+			return true;
+		}
+	}
+	// reduce window brightness if the player is dying
+	screen.darken_screen_factor = 1 - min_counter_ms / 3000;
+
+	return true;
+}
+
+
+
+// Reset the world state to its initial state
+void WorldSystem::restart_game() {
+	// Debugging for memory/component leaks
+	registry.list_all_components();
+	printf("Restarting\n");
+
+	// Reset the game speed
+	current_speed = 1.f;
+
+	// Remove all entities that we created
+	while (registry.motions.entities.size() > 0)
+	    registry.remove_all_components_of(registry.motions.entities.back());
+
+	// Debugging for memory/component leaks
+	registry.list_all_components();
+
+	// create a new player
+	player = createPlayer(renderer, { window_width_px/2, window_height_px - 200 });
+	registry.colors.insert(player, {1, 0.8f, 0.8f});
+
+}
+
+// Compute collisions between entities
+void WorldSystem::handle_collisions() {
+	// Loop over all collisions detected by the physics system
+	auto& collisionsRegistry = registry.collisions;
+	for (uint i = 0; i < collisionsRegistry.components.size(); i++) {
+		// The entity and its collider
+		Entity entity = collisionsRegistry.entities[i];
+		Entity entity_other = collisionsRegistry.components[i].other;
+
+		// for now, we are only interested in collisions that involve the player
+		if (registry.players.has(entity)) {
+			//Player& player = registry.players.get(entity);
+
+			// Checking Player - Deadly collisions
+			if (registry.robots.has(entity_other)) {
+				// initiate death unless already dying
+				if (!registry.deathTimers.has(entity)) {
+					// Scream, reset timer
+					registry.deathTimers.emplace(entity);
+					Mix_PlayChannel(-1, player_dead_sound, 0);
+
+			
+				}
+			}
+		}
+	}
+
+	// Remove all collisions from this simulation step
+	registry.collisions.clear();
+}
+
+// Should the game be over ?
+bool WorldSystem::is_over() const {
+	return bool(glfwWindowShouldClose(window));
+}
+
+// On key callback
+void WorldSystem::on_key(int key, int, int action, int mod) {
+	Motion& motion = registry.motions.get(player);
+	Inventory& inventory = registry.players.get(player).inventory;
+
+	if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+		switch (key) {
+		case GLFW_KEY_I:
+			// Toggle inventory open/close
+			inventory.isOpen = !inventory.isOpen;
+			if (inventory.isOpen) {
+				inventory.display(); // display inventory contents in console
+			}
+			else {
+				printf("Inventory closed.\n"); 
+			}
+			break;
+
+		case GLFW_KEY_ESCAPE:
+			// Close inventory if it is open
+			if (inventory.isOpen) {
+				inventory.isOpen = false; // Set inventory state to closed
+				printf("Inventory closed.\n"); // Close the inventory
+			}
+			break;
+
+			// Movement controls
+		case GLFW_KEY_W:
+			motion.velocity.y = -100.f; 
+			break;
+		case GLFW_KEY_S:
+			motion.velocity.y = 100.f;  
+			break;
+		case GLFW_KEY_A:
+			motion.velocity.x = -100.f; 
+			break;
+		case GLFW_KEY_D:
+			motion.velocity.x = 100.f; 
+			break;
+		}
+	}
+	else if (action == GLFW_RELEASE) {
+		switch (key) {
+		case GLFW_KEY_W:
+		case GLFW_KEY_S:
+			motion.velocity.y = 0.f; 
+			break;
+		case GLFW_KEY_A:
+		case GLFW_KEY_D:
+			motion.velocity.x = 0.f; 
+			break;
+		}
+	}
+
+
+	// Resetting game
+	if (action == GLFW_RELEASE && key == GLFW_KEY_R) {
+		int w, h;
+		glfwGetWindowSize(window, &w, &h);
+
+        restart_game();
+	}
+
+	// Debugging
+	if (key == GLFW_KEY_D) {
+		if (action == GLFW_RELEASE)
+			debugging.in_debug_mode = false;
+		else
+			debugging.in_debug_mode = true;
+	}
+
+	// Control the current speed with `<` `>`
+	if (action == GLFW_RELEASE && (mod & GLFW_MOD_SHIFT) && key == GLFW_KEY_COMMA) {
+		current_speed -= 0.1f;
+		printf("Current speed = %f\n", current_speed);
+	}
+	if (action == GLFW_RELEASE && (mod & GLFW_MOD_SHIFT) && key == GLFW_KEY_PERIOD) {
+		current_speed += 0.1f;
+		printf("Current speed = %f\n", current_speed);
+	}
+	current_speed = fmax(0.f, current_speed);
+}
+
+void WorldSystem::on_mouse_move(vec2 mouse_position) {
+
+	(vec2)mouse_position; // dummy to avoid compiler warning
+}
