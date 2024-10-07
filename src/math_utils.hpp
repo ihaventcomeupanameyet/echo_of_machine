4946cc82c545c81f378c2de51232271e05751b2c
UBC Student <ubcavi@students.cs.ubc.ca>
2024-10-07 00:18:33 -0700

    Linear interpolation for velocity

---
+++ src/math_utils.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+inline float linear_inter(float goal, float current, float dt) {
+	float difference = goal - current;
+
+	if (difference > dt) {
+		return current + dt;
+	}
+	if (difference < -dt) {
+		return current - dt;
+	}
+	return goal;
+}
