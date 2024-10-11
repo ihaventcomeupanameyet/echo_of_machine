18d2e1d30e161c3174f8dbd0cfd1a114cf577c24
UBC Student <adawar02@students.cs.ubc.ca>
2024-10-11 13:08:09 -0700

    "implemented the correct physics.cpp now

--- src/physics_system.cpp
+++ src/physics_system.cpp
@@ -1,89 +1,106 @@
 // internal
+/ internal
+#include "physics_system.hpp"
 #include "world_init.hpp"
-#include "tiny_ecs_registry.hpp"
 
-Entity createPlayer(RenderSystem* renderer, vec2 pos)
+// Returns the local bounding coordinates scaled by the current size of the entity
+vec2 get_bounding_box(const Motion& motion)
 {
-	auto entity = Entity();
+	// abs is to avoid negative scale due to the facing direction.
+	return { abs(motion.scale.x), abs(motion.scale.y) };
+}
 
-	// Setting initial motion values
-	Motion& motion = registry.motions.emplace(entity);
-	motion.position = pos;
-	motion.angle = 0.f;
-	motion.velocity = { 0.f, 0.f };
-	motion.scale = vec2({ PLAYER_BB_WIDTH, PLAYER_BB_HEIGHT });
-	//motion.scale.y *= -1; // point front to the right
+// This is a SUPER APPROXIMATE check that puts a circle around the bounding boxes and sees
+// if the center point of either object is inside the other's bounding-box-circle. You can
+// surely implement a more accurate detection
+bool collides(const Motion& motion1, const Motion& motion2)
+{
+    float scale_factor = 0.35;
 
-	// create an empty component for our character
-	Player& player = registry.players.emplace(entity);
+vec2 size1 = get_bounding_box(motion1) * scale_factor;
+vec2 size2 = get_bounding_box(motion2) * scale_factor;
 
-	Inventory& inventory = player.inventory;
-	// test items
-	inventory.addItem("Robot Hand", 2);
-	inventory.addItem("Key", 1);
+vec2 pos1_min = motion1.position - (size1 / 2.f); 
+vec2 pos1_max = motion1.position + (size1 / 2.f); 
 
+vec2 pos2_min = motion2.position - (size2 / 2.f);
+vec2 pos2_max = motion2.position + (size2 / 2.f);
 
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::PLAYER_IDLE,    
-		  EFFECT_ASSET_ID::TEXTURED,     
-		  GEOMETRY_BUFFER_ID::SPRITE });    
+bool overlap_x = (pos1_min.x <= pos2_max.x && pos1_max.x >= pos2_min.x);
+bool overlap_y = (pos1_min.y <= pos2_max.y - 10 && pos1_max.y >= pos2_min.y - 35);
 
+return overlap_x && overlap_y;
 
-	return entity;
 }
 
 
-Entity createRobot(RenderSystem* renderer, vec2 position)
+void PhysicsSystem::step(float elapsed_ms)
 {
-	auto entity = Entity();
+	ComponentContainer<Motion>& motion_container = registry.motions;
+	// Move entities based on the time passed, ensuring entities move at consistent speeds
+	auto& motion_registry = registry.motions;
+	for (uint i = 0; i < motion_registry.size(); i++)
+	{
+		Motion& motion = motion_registry.components[i];
+		Entity entity = motion_registry.entities[i];
+		float step_seconds = elapsed_ms / 1000.f;
+		vec2 pos = motion.position;
+		motion.position += motion.velocity * step_seconds;
+		vec2& position = motion.position;
 
-	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
-	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::SPRITE);
-	registry.meshPtrs.emplace(entity, &mesh);
 
-	// Initialize the motion
-	auto& motion = registry.motions.emplace(entity);
-	motion.angle = 0.f;
-	motion.velocity = { 0.f, 0.f };
-	motion.position = position;
-	motion.scale.y *= -1;
-
-	// Setting initial values, scale is negative to make it face the opposite way
-	motion.scale = vec2({ -ROBOT_BB_WIDTH, ROBOT_BB_HEIGHT });
-
-	// create an empty Robot component to be able to refer to all robots
-	registry.robots.emplace(entity);
-	registry.renderRequests.insert(
-		entity,
+		Motion& motion_i = motion_container.components[i];
+		Entity entity_i = motion_container.entities[i];
+		for (uint j = i + 1; j < motion_container.components.size(); j++)
 		{
-			TEXTURE_ASSET_ID::ROBOT,
-			EFFECT_ASSET_ID::TEXTURED,
-			GEOMETRY_BUFFER_ID::SPRITE
-		});
+			//Motion& motion_i = motion_container.components[i];
+			//Entity entity_i = motion_container.entities[i];
+			Motion& motion_j = motion_container.components[j];
+			if (collides(motion_i, motion_j))
+			{
+				Entity entity_j = motion_container.entities[j];
 
-	return entity;
-}
+				// Set velocities to zero when collision happens
+				motion_i.velocity = { 0.f, 0.f };
+				motion_j.velocity = { 0.f, 0.f };
 
-Entity createLine(vec2 position, vec2 scale)
-{
-	Entity entity = Entity();
+				// Create collision events
+				registry.collisions.emplace_with_duplicates(entity_i, entity_j);
+				registry.collisions.emplace_with_duplicates(entity_j, entity_i);
+				position = pos;
 
-	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
-	//registry.renderRequests.insert(
-	//	entity, {
-	//		TEXTURE_ASSET_ID::TEXTURE_COUNT,
-	//		EFFECT_ASSET_ID::EGG,
-	//		GEOMETRY_BUFFER_ID::DEBUG_LINE
-	//	});
+			}
+		}
+		// Clamp the position to the screen boundaries
+		position.x = fmax(16.f, fmin(position.x, window_width_px-16));
+		position.y = fmax(0.f, fmin(position.y, window_height_px-32));
+	}
 
-	// Create motion
-	Motion& motion = registry.motions.emplace(entity);
-	motion.angle = 0.f;
-	motion.velocity = { 0, 0 };
-	motion.position = position;
-	motion.scale = scale;
+	// Check for collisions between all moving entities
 
-	registry.debugComponents.emplace(entity);
-	return entity;
+	for (uint i = 0; i < motion_container.components.size(); i++)
+	{
+		Motion& motion_i = motion_container.components[i];
+		Entity entity_i = motion_container.entities[i];
+
+		// Note: starting j at i+1 to compare all (i,j) pairs only once (and to avoid self-comparison)
+		for (uint j = i + 1; j < motion_container.components.size(); j++)
+		{
+			Motion& motion_j = motion_container.components[j];
+			if (collides(motion_i, motion_j))
+			{
+				Entity entity_j = motion_container.entities[j];
+
+				// Set velocities to zero when collision happens
+				motion_i.velocity = { 0.f, 0.f };
+				motion_j.velocity = { 0.f, 0.f };
+
+				// Create collision events
+				registry.collisions.emplace_with_duplicates(entity_i, entity_j);
+				registry.collisions.emplace_with_duplicates(entity_j, entity_i);
+
+
+			}
+		}
+	}
 }
