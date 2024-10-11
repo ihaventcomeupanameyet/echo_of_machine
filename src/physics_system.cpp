db498e3639b014ef4987df4ec208e8b32f34d435
UBC Student <adawar02@students.cs.ubc.ca>
2024-10-11 12:49:05 -0700

    "updated collisions"

--- src/physics_system.cpp
+++ src/physics_system.cpp
@@ -1,71 +1,89 @@
 // internal
-#include "physics_system.hpp"
 #include "world_init.hpp"
-#include "math_utils.hpp"
+#include "tiny_ecs_registry.hpp"
 
-// Returns the local bounding coordinates scaled by the current size of the entity
-vec2 get_bounding_box(const Motion& motion)
+Entity createPlayer(RenderSystem* renderer, vec2 pos)
 {
-	// abs is to avoid negative scale due to the facing direction.
-	return { abs(motion.scale.x), abs(motion.scale.y) };
+	auto entity = Entity();
+
+	// Setting initial motion values
+	Motion& motion = registry.motions.emplace(entity);
+	motion.position = pos;
+	motion.angle = 0.f;
+	motion.velocity = { 0.f, 0.f };
+	motion.scale = vec2({ PLAYER_BB_WIDTH, PLAYER_BB_HEIGHT });
+	//motion.scale.y *= -1; // point front to the right
+
+	// create an empty component for our character
+	Player& player = registry.players.emplace(entity);
+
+	Inventory& inventory = player.inventory;
+	// test items
+	inventory.addItem("Robot Hand", 2);
+	inventory.addItem("Key", 1);
+
+
+	registry.renderRequests.insert(
+		entity,
+		{ TEXTURE_ASSET_ID::PLAYER_IDLE,    
+		  EFFECT_ASSET_ID::TEXTURED,     
+		  GEOMETRY_BUFFER_ID::SPRITE });    
+
+
+	return entity;
 }
 
-// This is a SUPER APPROXIMATE check that puts a circle around the bounding boxes and sees
-// if the center point of either object is inside the other's bounding-box-circle. You can
-// surely implement a more accurate detection
-bool collides(const Motion& motion1, const Motion& motion2)
+
+Entity createRobot(RenderSystem* renderer, vec2 position)
 {
-	vec2 dp = motion1.position - motion2.position;
-	float dist_squared = dot(dp,dp);
-	const vec2 other_bonding_box = get_bounding_box(motion1) / 2.f;
-	const float other_r_squared = dot(other_bonding_box, other_bonding_box);
-	const vec2 my_bonding_box = get_bounding_box(motion2) / 2.f;
-	const float my_r_squared = dot(my_bonding_box, my_bonding_box);
-	const float r_squared = max(other_r_squared, my_r_squared);
-	if (dist_squared < r_squared)
-		return true;
-	return false;
-}
+	auto entity = Entity();
 
-void PhysicsSystem::step(float elapsed_ms)
-{
-	// Move fish based on how much time has passed, this is to (partially) avoid
-	// having entities move at different speed based on the machine.
-	auto& motion_registry = registry.motions;
-	for(uint i = 0; i< motion_registry.size(); i++)
-	{
-		
-		Motion& motion = motion_registry.components[i];
-		Entity entity = motion_registry.entities[i];
-		float step_seconds = elapsed_ms / 1000.f;
+	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
+	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::SPRITE);
+	registry.meshPtrs.emplace(entity, &mesh);
 
-		motion.velocity.x = linear_inter(motion.target_velocity.x, motion.velocity.x, step_seconds * 100.0f);
-		motion.velocity.y = linear_inter(motion.target_velocity.y, motion.velocity.y, step_seconds * 100.0f);
-		motion.position += motion.velocity * step_seconds;
-		
-	}
+	// Initialize the motion
+	auto& motion = registry.motions.emplace(entity);
+	motion.angle = 0.f;
+	motion.velocity = { 0.f, 0.f };
+	motion.position = position;
+	motion.scale.y *= -1;
 
+	// Setting initial values, scale is negative to make it face the opposite way
+	motion.scale = vec2({ -ROBOT_BB_WIDTH, ROBOT_BB_HEIGHT });
 
-	// Check for collisions between all moving entities
-    ComponentContainer<Motion> &motion_container = registry.motions;
-	for(uint i = 0; i<motion_container.components.size(); i++)
-	{
-		Motion& motion_i = motion_container.components[i];
-		Entity entity_i = motion_container.entities[i];
-		
-		// note starting j at i+1 to compare all (i,j) pairs only once (and to not compare with itself)
-		for(uint j = i+1; j<motion_container.components.size(); j++)
+	// create an empty Robot component to be able to refer to all robots
+	registry.robots.emplace(entity);
+	registry.renderRequests.insert(
+		entity,
 		{
-			Motion& motion_j = motion_container.components[j];
-			if (collides(motion_i, motion_j))
-			{
-				Entity entity_j = motion_container.entities[j];
-				// Create a collisions event
-				// We are abusing the ECS system a bit in that we potentially insert muliple collisions for the same entity
-				registry.collisions.emplace_with_duplicates(entity_i, entity_j);
-				registry.collisions.emplace_with_duplicates(entity_j, entity_i);
-			}
-		}
-	}
+			TEXTURE_ASSET_ID::ROBOT,
+			EFFECT_ASSET_ID::TEXTURED,
+			GEOMETRY_BUFFER_ID::SPRITE
+		});
 
+	return entity;
+}
+
+Entity createLine(vec2 position, vec2 scale)
+{
+	Entity entity = Entity();
+
+	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
+	//registry.renderRequests.insert(
+	//	entity, {
+	//		TEXTURE_ASSET_ID::TEXTURE_COUNT,
+	//		EFFECT_ASSET_ID::EGG,
+	//		GEOMETRY_BUFFER_ID::DEBUG_LINE
+	//	});
+
+	// Create motion
+	Motion& motion = registry.motions.emplace(entity);
+	motion.angle = 0.f;
+	motion.velocity = { 0, 0 };
+	motion.position = position;
+	motion.scale = scale;
+
+	registry.debugComponents.emplace(entity);
+	return entity;
 }
