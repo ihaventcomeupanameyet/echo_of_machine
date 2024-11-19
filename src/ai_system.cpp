0a2dbef726410e7db0fb8326ac831e54a764091a
UBC Student <adawar02@students.cs.ubc.ca>
2024-11-19 12:24:07 -0800

    Delete src directory

--- src/ai_system.cpp
+++
@@ -1,8 +0,0 @@
-// internal
-// robot ai will be shifted here
-#include "ai_system.hpp"
-
-void AISystem::step(float elapsed_ms)
-{
-	(void)elapsed_ms; // placeholder to silence unused warning until implemented
-}
