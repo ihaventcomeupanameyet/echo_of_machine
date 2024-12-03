96d6692f996603f5918f0e82ee4c0b7445e09343
UBC Student <771plant@gmail.com>
2024-12-02 23:52:54 -0800

    right order

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -3144,7 +3144,7 @@ void WorldSystem::load_level(int level) {
 
 		load_tutorial_level(20, 12);
 		break;
-	case 5:
+	case 1:
 		//registry.maps.clear();
 		map_width = 21;
 		map_height = 18;
@@ -3191,7 +3191,7 @@ void WorldSystem::load_level(int level) {
 		//generate_json(registry);
 		break;
 
-	case 1:
+	case 5:
 		// Setup for final level
 		registry.maps.clear();
 		map_width = 64;
